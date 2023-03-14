e6044e0292006da02d2d88d9a49a95075c92495a
wild334month <9zc9@fresnoU.org>
2023-03-14 10:05:52 -0700

    initial commit

---
+++ functionNotes.js
@@ -0,0 +1,37 @@
+//Function Notes
+//Step 1:Define the function
+function sayHello(){
+    console.log('Hello');
+}
+//Step2:Call (or use) the function
+//sayHello();
+
+//function printMax(){
+   // let x=6;
+    //let y=29;
+    //if (x>y) {
+      //  console.log(`The ;arger number is ${x}.`);
+    //}else {
+     //   console.log(`The larger number is ${y}.`);
+    //}
+//}
+
+
+
+
+//function printMax(x,y) {
+    //if (x>y) {
+      //  console.log(`The larger number is ${x}.`);
+    //}else{
+    //    console.log(`The l;arger number is ${y}.`);
+  //  }
+//}
+//printMax(x:16, y:9);
+//printMax(x:2,y:75);
+//printMax(x:2,y:75);
+
+function sum(num1, num2){
+    console.log(`${num1}+${num2}=${num1}+${num2}`);
+}
+sum(7, 10);
+sum(27, 1);
